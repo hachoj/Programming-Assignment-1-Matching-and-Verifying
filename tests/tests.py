ebcb48592df06063676f94c3abaf08f5ca9929f8
hachoj <hjchojnowski@gmail.com>
2026-01-29 17:42:23 -0500

    Small commit to push branch to remote

--- tests/tests.py
+++ tests/tests.py
@@ -2,3 +2,9 @@ def test_example():
     a = 3
     b = 3
     assert a is b, "a should have the same memory location as b"
+
+
+def test_example_2():
+    a = 3.1
+    b = -3.1
+    assert a is not b, "a should have a different memory location then b"
